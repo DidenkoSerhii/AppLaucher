31402721bb332e4830750c17b3a02861e37b3cf4
vlshutyak <vl.shutyak@gmail.com>
2024-03-26 21:48:56 +0200

    Notification time implemented

--- src/main/java/org/CurrencyExchangeRateBot/Buttons.java
+++ src/main/java/org/CurrencyExchangeRateBot/Buttons.java
@@ -31,4 +31,11 @@ public class Buttons {
         return InlineKeyboardMarkupBuilder.buildQuantityOfNumbersMarkup(
                 new String[]{"2", "3", "4"}, chatId);
     }
+    public static InlineKeyboardMarkup timeIntervals(long chatId) {
+        return InlineKeyboardMarkupBuilder.buildTimeOfNotificationMarkup(
+                new String[]{" 9:00","10:00","11:00",
+                             "12:00","13:00","14:00",
+                             "15:00","16:00","17:00",
+                             "18:00","Вимкнути сповіщення"},chatId);
+    }
 }
